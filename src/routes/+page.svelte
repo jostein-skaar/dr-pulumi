2ed7bfb2512910e3058e927c039c8c9388c66b3a
Jostein Skaar <jostein.skaar@gmail.com>
2024-12-18 15:40:36 +0100

    feat: basic page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+import { fade, fly } from "svelte/transition";
+import drPulumiImage from "$lib/images/dr-pulumi.png";
+import { onMount } from "svelte";
+
+// const rank = localStorage.getItem("drpulumi-rank") || "Pulumi Noob";
+
+let visible = false;
+onMount(() => {
+	visible = true;
+});
+</script>
+
+{#if visible}
+	<header>
+		<h1 transition:fly={{ y: -40, duration: 2000 }}>Dr Pulumi</h1>
+		<p transition:fade={{ duration: 1000, delay: 200 }}>
+			A long time ago, in the age when every infrastructure change required frantic button-clicking, there emerged a man, a legend: Dr. Pulumi. His brilliance and innovation would forever redefine the destiny of the Web Team.
+		</p>
+		<img
+			transition:fly={{ x: -1000, duration: 1000, delay: 0 }}
+			src={drPulumiImage}
+			alt="Drawing of Dr Pulumi"
+		/>
+		<!-- <p class="rank">Current Rank: <span>{rank}</span></p> -->
+
+		<a transition:fly={{ y: 100, duration: 2000 }} href="/game">Let's get started</a>
+	</header>
+{/if}
+
+<style>
+	/* .rank span {
+		font-size: 1.5rem;
+	} */
+</style>
---
+++ src/routes/lose/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { fade } from "svelte/transition";
+import drPulumiImage from "$lib/images/dr-pulumi.png";
+import { onMount } from "svelte";
+
+let visible = false;
+onMount(() => {
+	visible = true;
+});
+</script>
+
+{#if visible}
+<header transition:fade={{ duration: 1000, delay: 0 }}>
+	<h1>Dr Pulumi</h1>
+
+		<p>Don't give up!</p>	
+
+		<img src={drPulumiImage} alt="Drawing of Dr Pulumi" />
+		<a href="/game">Try Again</a>
+	</header>
+{/if}
